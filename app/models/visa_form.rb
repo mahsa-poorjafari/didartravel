b2ab39212b81d19913aabafc3dda5dfea4566b64
mahsa <m_mw7462@yahoo.com>
2016-01-05 12:24:49 +0100

    fix visa form show

--- app/models/visa_form.rb
+++ app/models/visa_form.rb
@@ -3,6 +3,7 @@ class VisaForm < ActiveRecord::Base
   has_many :passengers
   has_many :hosts  
   accepts_nested_attributes_for :hosts, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :passengers, reject_if: :all_blank, allow_destroy: true
   validates :I_agree, :presence => {:message => 'Agree the Visa process agreement'}
   
   
