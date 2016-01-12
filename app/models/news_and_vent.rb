8cdb31dce07ca95173c0a9cec148af570154854c
mahsa <m_mw7462@yahoo.com>
2016-01-12 13:30:19 +0100

    increas size of image for events to 500kb

--- app/models/news_and_vent.rb
+++ app/models/news_and_vent.rb
@@ -5,7 +5,7 @@ class NewsAndVent < ActiveRecord::Base
   has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "100x150>", :toursize => "248x370#", :tumb => "120x70" }
   validates_attachment :image, 
     :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"], :message => "فرمت عکس صحیح می باشد." },
-    :size => { :in => 0..300.kilobytes , :message => "حجم تصویر بیشتر از 300 کیلوبایت است."}
+    :size => { :in => 0..500.kilobytes , :message => "حجم تصویر بیشتر از500 کیلوبایت است."}
   def title
     if I18n.locale == :ar
       self.read_attribute("title_ar")
