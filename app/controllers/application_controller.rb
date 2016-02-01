9141b27f79b009990c801f1250e4c837c834d7cd
mahsa <m_mw7462@yahoo.com>
2016-02-01 11:28:13 +0100

    deactivated pass for enroll in site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception  
   before_filter :set_locale, :online_info
   before_filter :load_slides
-  before_filter :authenticate
+  #before_filter :authenticate
   include SimpleCaptcha::ControllerHelpers
   protected
     def check_autentication
